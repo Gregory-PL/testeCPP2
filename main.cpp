6c099a270a43d1d1f7b1c1a6b7536aa800911767
chrome <gregoryus3@gmail.com>
2014-01-31 21:45:29 -0200

    add more code
    cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "GitHub Windows client." << endl;
+    cout << "GitHub Windows client2." << endl;
+    cout << "GitHub Windows client3." << endl;
     return 0;
 }
